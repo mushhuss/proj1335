b117ec31c66c3a388b49d87232b934b8ec408267
Muslim Hussaini <hussainimuslim2@gmail.com>
2024-10-06 20:59:29 -0400

    6

--- test.cpp
+++ test.cpp
@@ -8,6 +8,7 @@ Muslim Hussaini
 
 int main() {
 
+    // Create files with different names, and copying/moving various files
     File file1; 
     File file2(".env", "aaaaa");
     File file3("aaa", "Content");
@@ -17,15 +18,10 @@ int main() {
     File file7 = File("hello.png");
     file7 = file1;
 
-    std::cout <<file1.getSize() <<std::endl;
-    std::cout << file2.getSize() <<std::endl;
-    std::cout << file3.getSize() <<std::endl;
-    std::cout << file4.getSize() <<std::endl;
-    std::cout << file5.getSize() <<std::endl;
-    std::cout << file6.getSize() <<std::endl;
-    std::cout << file7.getSize() <<std::endl;
-
+    // check names of each file
     std::cout << file1.getName() << std::endl;
+    std::cout << file2.getName() << std::endl;
+    std::cout << file3.getName() << std::endl;
     std::cout << file4.getName() <<std::endl;
     std::cout << file5.getName() <<std::endl;
     std::cout << file6.getName() <<std::endl;
