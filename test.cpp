d3b4ebb66eff86634ada21823313a285f2893885
Muslim Hussaini <hussainimuslim2@gmail.com>
2024-10-08 12:00:06 -0400

    last

--- test.cpp
+++ test.cpp
@@ -9,28 +9,52 @@ Muslim Hussaini
 int main() {
 
     // Create files with different names, and copying/moving various files
-    File file1; 
-    File file2(".jpeg", "hi");
-    File file3("bb", "random");
-    File file4 = File(file1);
-    File file5 = file3;
-    File file6 = std::move(file1);
-    File file7 = File("bruh.jpeg");
-    file7 = file1;
-    File file8("abc.");
-    File file9;
+    File one; 
+    File two(".jpeg", "hi");
+    File three("bb", "random");
+    File four = File(one);
+    File five = three;
+    File six = std::move(one);
+    File seven = File("bruh.jpeg");
+    seven = one;
+    File eight("abc.");
+    File nine;
 
     // check names of each file
-    std::cout << file1.getName() << std::endl;
-    std::cout << file2.getName() << std::endl;
-    std::cout << file3.getName() << std::endl;
-    std::cout << file4.getName() <<std::endl;
-    std::cout << file5.getName() <<std::endl;
-    std::cout << file6.getName() <<std::endl;
-    std::cout << file7.getName() << std::endl;
-    std::cout << file8.getName() << std::endl;
-    std::cout << file9.getName() << std::endl;
+std::cout << one.getSize() << std::endl;
+std::cout << two.getSize() << std::endl;
+std::cout << three.getSize() << std::endl;
+std::cout << four.getSize() << std::endl;
+std::cout << five.getSize() << std::endl;
+std::cout << six.getSize() << std::endl;
+std::cout << seven.getSize() << std::endl;
+std::cout << eight.getSize() << std::endl;
+std::cout << nine.getSize() << std::endl;
 
+std::cout << one.getName() << std::endl;
+std::cout << two.getName() << std::endl;
+std::cout << three.getName() << std::endl;
+std::cout << four.getName() << std::endl;
+std::cout << five.getName() << std::endl;
+std::cout << six.getName() << std::endl;
+std::cout << seven.getName() << std::endl;
+std::cout << eight.getName() << std::endl;
+std::cout << nine.getName() << std::endl;
+
+    Folder source;
+    source.display();
+    Folder destination;
+
+    source.addFile(one);
+    source.moveFileTo(one.getName(), destination);
+    destination.removeFile(one.getName());
+
+    Folder f1;
+    Folder f2("Folder");
+
+
+
+    
     return 0;
 }
 
